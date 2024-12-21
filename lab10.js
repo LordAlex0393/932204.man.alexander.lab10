9af6ba85a5f082ff97e843c9d6e349785e05cd3c
Lord_Alex <alexstone0390@gmail.com>
2024-12-21 12:58:58 +0700

    v1.0 - release

---
+++ lab10.js
@@ -0,0 +1,27 @@
+function sliden(){
+    document.getElementById('backstage').id = 'slidebackstage';
+}
+
+function on(){
+    if(document.getElementById('off')){
+        document.getElementById('off').id = 'light_on';
+        document.getElementById('fairy').style.display = 'block';
+        document.getElementById('foc').style.display = 'block';
+    }
+
+    else{
+        document.getElementById('light_on').id = 'off';
+        document.getElementById('fairy').style.display = 'none';
+        document.getElementById('foc').style.display = 'none';
+    }
+}
+
+function focusing(){
+    document.getElementById('rabbit').id = 'rabbit_new';
+    document.getElementById('pigeon_new').id = 'pigeon';
+}
+
+function pigeon(){
+    document.getElementById('pigeon').id = 'pigeon_new';
+    document.getElementById('rabbit_new').id = 'rabbit';
+}
